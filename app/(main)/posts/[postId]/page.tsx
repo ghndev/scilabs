59b6089c2849d55a8d417ac3d66f5aff231aba5c
Kwak Hoon <77324409+hooncdn@users.noreply.github.com>
2024-10-22 00:16:58 +0900

    feat: Add cache feature

--- app/(main)/posts/[postId]/page.tsx
+++ app/(main)/posts/[postId]/page.tsx
@@ -3,6 +3,7 @@ import { PostDetail } from './post-detail'
 import { getPostDataInclude } from '@/lib/types'
 import { validateRequest } from '@/auth'
 import { notFound } from 'next/navigation'
+import { unstable_cache as nextCache } from 'next/cache'
 
 interface PageProps {
   params: { postId: string }
@@ -19,8 +20,13 @@ async function getPost(postId: string, userId?: string) {
   return post
 }
 
+const getCachedPost = nextCache(getPost, ['post'], {
+  tags: ['post'],
+  revalidate: 60
+})
+
 export async function generateMetadata({ params: { postId } }: PageProps) {
-  const post = await getPost(postId)
+  const post = await getCachedPost(postId)
 
   if (!post) {
     return { title: 'Post not found' }
@@ -31,7 +37,7 @@ export async function generateMetadata({ params: { postId } }: PageProps) {
 
 export default async function Page({ params: { postId } }: PageProps) {
   const { user } = await validateRequest()
-  const post = await getPost(postId, user?.id)
+  const post = await getCachedPost(postId, user?.id)
 
   if (!post) {
     return notFound()
