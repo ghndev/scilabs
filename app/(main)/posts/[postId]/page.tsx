f233f12e2940081f59a661e10c937fdb29582cbc
Kwak Hoon <77324409+hooncdn@users.noreply.github.com>
2024-10-23 13:17:15 +0900

    refactor: Only speed

--- app/(main)/posts/[postId]/page.tsx
+++ app/(main)/posts/[postId]/page.tsx
@@ -6,28 +6,28 @@ interface PageProps {
   params: { postId: string }
 }
 
-async function getPost(postId: string) {
-  const post = db.post.findUnique({
-    where: {
-      id: postId
-    }
-  })
+// async function getPost(postId: string) {
+//   const post = db.post.findUnique({
+//     where: {
+//       id: postId
+//     }
+//   })
 
-  return post
-}
+//   return post
+// }
 
-const getCachedPost = nextCache(getPost, ['post'], {
-  tags: ['post'],
-  revalidate: 60
-})
+// const getCachedPost = nextCache(getPost, ['post'], {
+//   tags: ['post'],
+//   revalidate: 60
+// })
 
-export async function generateMetadata({ params: { postId } }: PageProps) {
-  const post = await getCachedPost(postId)
-  if (!post) {
-    return { title: 'Post not found' }
-  }
-  return { title: `sciLABS | ${post.title}` }
-}
+// export async function generateMetadata({ params: { postId } }: PageProps) {
+//   const post = await getCachedPost(postId)
+//   if (!post) {
+//     return { title: 'Post not found' }
+//   }
+//   return { title: `sciLABS | ${post.title}` }
+// }
 
 export default async function Page({ params: { postId } }: PageProps) {
   return <PostDetail postId={postId} />
